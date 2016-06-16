2e5cc776b2705f8f3675c778dbe3394d86c3b90b
dassir <dassir@wix.com>
2016-06-16 05:11:00 +0300

    profile styling

--- public/scripts/sitterP.js
+++ public/scripts/sitterP.js
@@ -20,9 +20,11 @@ var ReviewsList = React.createClass({
               <div className="icon-container">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125"><path fill="#ffe754"  d="M77 90.4L50 76.2 23 90.4l5.2-30L6.4 39.1l30.1-4.4L50 7.4l13.5 27.3 30.1 4.4L71.8 60.4 77 90.4zM27.9 46.1L38.9 56.9l-2.6 15.2L50 64.9l13.7 7.2L61.1 56.9l11.1-10.8-15.3-2.2L50 30l-6.8 13.9L27.9 46.1z"/></svg>
               </div>
-              <p className="reviewRating">{review.rating} </p>
-              <p className="reviewName">{review.parentName} </p>
-              <p className="reviewDate">{review.date}</p>
+              <section class="review-details">
+                <p className="reviewRating">{review.rating} </p>
+                <p className="reviewName">{review.parentName} </p>
+                <p className="reviewDate">{review.date}</p>
+              </section>
             </section>
           </section>
           <p className="review-text">{review.review}</p>
