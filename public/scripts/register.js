8521fc866921cc39b774bc90e98d1ca902280157
ArelGindos <arel-g@hotmail.com>
2016-06-30 22:31:33 +0300

    added register sitter and parent css+js+html

---
+++ public/scripts/register.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Arel on 29/06/2016.
+ */
+
+$('.multi-field-wrapper').each(function() {
+  var $wrapper = $('.multi-fields', this);
+  $(".add-field", $(this)).click(function(e) {
+    $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('input').val('').focus();
+  });
+  $('.multi-field .remove-field', $wrapper).click(function() {
+    if ($('.multi-field', $wrapper).length > 1)
+      $(this).parent('.multi-field').remove();
+  });
+});
+
+$('#controller input').on('change', function() {
+  var type = $('input[name="radiochoose"]:checked', '#controller').val();
+  if(type == "parent"){
+    document.getElementById('sitter-register').style.display = "none";
+    document.getElementById('parent-register').style.display = "inline";
+  }
+  else{
+    document.getElementById('parent-register').style.display = "none";
+    document.getElementById('sitter-register').style.display = "inline";
+  }
+
+});
