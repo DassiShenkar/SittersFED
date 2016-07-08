2bba72cce82a546a1c0d26fce0d910193462a5fb
DassiShenkar <dassir@wix.com>
2016-07-08 19:18:32 +0300

    deleted anwanted files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 
 
-app.set('port', (process.env.PORT || 4000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
