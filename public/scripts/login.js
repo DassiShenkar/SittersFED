327d51f7204a6af4c11cb011741a42eb22b1ed72
dassiShenkar <dassir@wix.com>
2016-07-09 23:48:55 +0300

    updated login and register and changed ajax call to heroku adress

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -4,9 +4,7 @@ var LoginForm = React.createClass({
   render: function () {
     return (
       <div id="login-form">
-        <div id="logo-container">
-          <img id="logo" src="./static/images/logo-sitters.png" alt="sitters logo" title="sitters-logo"/>
-        </div>
+        <img id="logo" src="./static/images/logo_sitters_blue.png" alt="sitters logo" title="sitters-logo"/>
         <h1>Sign In</h1>
         <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
       </div>
